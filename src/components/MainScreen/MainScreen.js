34e1722694c24effa6eca8690a45a8ced9f0b036
tania <tanakorchynska@gmail.com>
2023-10-24 19:52:49 -0400

    Added animated css to the main screen

--- src/components/MainScreen/MainScreen.js
+++ src/components/MainScreen/MainScreen.js
@@ -1,18 +1,60 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./MainScreen.css";
 
 const MainScreen = () => {
+  const [loaded, setLoaded] = useState(false);
+  const [animateBtn, setAnimateBtn] = useState(false);
+  const [animateH3, setAnimateH3] = useState(false);
+  const [animateH2, setAnimateH2] = useState(false);
+  const [animateH1, setAnimateH1] = useState(false);
+
+  useEffect(() => {
+    const loadTimer = setTimeout(() => {
+      setLoaded(true);
+
+      // Start the animation sequence
+      setAnimateBtn(true);
+      const timerH3 = setTimeout(() => setAnimateH3(true), 100);
+      const timerH2 = setTimeout(() => setAnimateH2(true), 200);
+      const timerH1 = setTimeout(() => setAnimateH1(true), 300);
+
+      // Cleanup the animation timers
+      return () => {
+        clearTimeout(timerH3);
+        clearTimeout(timerH2);
+        clearTimeout(timerH1);
+      };
+    }, 300);
+
+    // Cleanup the load timer
+    return () => {
+      clearTimeout(loadTimer);
+    };
+  }, []);
+
+  if (!loaded) {
+    return <div className="while-loading"></div>;
+  }
+
   return (
     <section className="home">
-      <h1 className="home-h1">Hello, I am</h1>
-      <h2 className="home-h2">Tetiana Korchynska</h2>
-      <h3 className="home-h3">Front-End Developer & UI/UX Designer</h3>
-      <a href="https://www.google.com/">
-        <img
-          src="https://s3.amazonaws.com/shecodesio-production/uploads/files/000/101/753/original/icon-down.png?1698114371"
-                  alt="down"
-                  className="home-icon"
-        />
+      <h1 className={`home-h1 ${animateH1 ? "animate" : ""}`}>Hello, I am</h1>
+      <h2 className={`home-h2 ${animateH2 ? "animate" : ""}`}>
+        Tetiana Korchynska
+      </h2>
+      <h3 className={`home-h3 ${animateH3 ? "animate" : ""}`}>
+        Front-End Developer & UI/UX Designer
+      </h3>
+      <a
+        href="https://www.google.com/"
+        className={`home-btn ${animateBtn ? "animate" : ""}`}
+        id="animated-btn"
+      >
+        <svg width="180px" height="60px" viewBox="0 0 180 60" class="border">
+          <polyline points="179,1 179,59 1,59 1,1 179,1" class="bg-line" />
+          <polyline points="179,1 179,59 1,59 1,1 179,1" class="hl-line" />
+        </svg>
+        <span>More</span>
       </a>
     </section>
   );
