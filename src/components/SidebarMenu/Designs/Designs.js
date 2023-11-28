6f49a1c42f5302b042bbae63629a214891d8953e
tania <tanakorchynska@gmail.com>
2023-11-27 22:02:14 -0500

    Renamed component

--- src/components/SidebarMenu/Designs/Designs.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import "./Designs.css";
-
-
-function Designs() {
-  return <h1>hello Designs</h1>;
-}
-
-export default Designs;
---
+++ src/components/SidebarMenu/Experience/Experience.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Experience.css";
+
+
+function Experience() {
+  return (
+    <h1>hello</h1>
+  );
+}
+
+export default Experience;
