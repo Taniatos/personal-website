b0eec7946004ffa7fd95e3c2bfa0ba664cf111a6
tania <tanakorchynska@gmail.com>
2023-11-28 14:05:24 -0500

    Added experience page

--- src/components/SidebarMenu/Experience/Experience.js
+++ src/components/SidebarMenu/Experience/Experience.js
@@ -1,10 +1,158 @@
 import React from "react";
 import "./Experience.css";
 
-
 function Experience() {
   return (
-    <h1>hello</h1>
+    <div className="experience-page">
+      <h1 className="experience-page-header">Relevant work experience</h1>
+      <div class="accordion" id="accordionPanelsStayOpenExample">
+        <div class="accordion-item">
+          <h2 class="accordion-header">
+            <button
+              class="accordion-button"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#panelsStayOpen-collapseOne"
+              aria-expanded="true"
+              aria-controls="panelsStayOpen-collapseOne"
+            >
+              Junior Front-End Developer
+            </button>
+          </h2>
+          <div
+            id="panelsStayOpen-collapseOne"
+            class="accordion-collapse collapse show"
+          >
+            <div class="accordion-body">
+              <a
+                href="https://www.wikibusines.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="experience-company-name"
+              >
+                Wikibusines
+              </a>
+              <h5 className="experience-years">Jun 2023 - present</h5>
+              <ul className="experience-ul">
+                <li className="experience-li">
+                  contributing to the development of client websites and landing
+                  pages by designing, writing, and implementing code snippets
+                  and components (HTML, CSS, JS, React)
+                </li>
+                <li className="experience-li">
+                  {" "}
+                  collaborating with the team to enhance website functionality
+                  and user experience
+                </li>
+                <li className="experience-li">
+                  troubleshooting and debugging issues
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div class="accordion-item">
+          <h2 class="accordion-header">
+            <button
+              class="accordion-button collapsed"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#panelsStayOpen-collapseTwo"
+              aria-expanded="false"
+              aria-controls="panelsStayOpen-collapseTwo"
+            >
+              HR & Reputation Manager
+            </button>
+          </h2>
+          <div
+            id="panelsStayOpen-collapseTwo"
+            class="accordion-collapse collapse"
+          >
+            <div class="accordion-body">
+              <a
+                href="https://trionika.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="experience-company-name"
+              >
+                Trionika
+              </a>
+              <h5 className="experience-years">Feb 2019 - Apr 2023</h5>
+              <ul className="experience-ul">
+                <li className="experience-li">
+                  recruiting and searching for new partners for specific
+                  projects
+                </li>
+                <li className="experience-li">
+                  {" "}
+                  improving the website's reputation - professional processing
+                  of user reviews on popular platforms, including Sitejabber,
+                  Glassdoor, and Trustpilot; motivation of partners to leave
+                  feedback regarding cooperation
+                </li>
+                <li className="experience-li">
+                  designing content in Figma and creating monthly content plans
+                  for the company's corporate pages on Facebook, Twitter, and
+                  Instagram
+                </li>
+                <li className="experience-li">
+                  writing technical tasks for the IT Department
+                </li>
+                <li className="experience-li">
+                  participation in strategic sessions for drawing up a company
+                  development plan and product & service improvement,
+                  identification of risks and problems, as well as generation of
+                  ideas for their elimination
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div class="accordion-item">
+          <h2 class="accordion-header">
+            <button
+              class="accordion-button collapsed"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#panelsStayOpen-collapseThree"
+              aria-expanded="false"
+              aria-controls="panelsStayOpen-collapseThree"
+            >
+              Customer Support Representative
+            </button>
+          </h2>
+          <div
+            id="panelsStayOpen-collapseThree"
+            class="accordion-collapse collapse"
+          >
+            <div class="accordion-body">
+              <a
+                href="https://trionika.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="experience-company-name"
+              >
+                Trionika
+              </a>
+              <h5 className="experience-years">Sep 2017 - Feb 2019</h5>
+              <ul className="experience-ul">
+                <li className="experience-li">
+                  management of customers' orders from the moment of their
+                  creation to completion and customer support throughout this
+                  entire cycle
+                </li>
+                <li className="experience-li">
+                  providing assistance via live chat, email, and over the phone
+                </li>
+                <li className="experience-li">
+                  mentoring new members of the team
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
