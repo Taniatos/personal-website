e031c2f4405867dc55d9ec2754d8fc0f4f077cfb
tania <tanakorchynska@gmail.com>
2023-11-28 20:24:05 -0500

    Update About.js

--- src/components/SidebarMenu/About/About.js
+++ src/components/SidebarMenu/About/About.js
@@ -73,6 +73,7 @@ function About() {
               <a
                 href="https://www.shecodes.io/certificates/67082-tetiana-korchynska/max"
                 target="_blank"
+                rel="noopener noreferrer"
               >
                 <img
                   src="https://s3.amazonaws.com/shecodesio-production/uploads/files/000/084/758/original/10.png?1686165153"
@@ -83,6 +84,7 @@ function About() {
               <a
                 href="https://www.shecodes.io/certificates/c59b5f960e9b9c588c8b340d3b415f48"
                 target="_blank"
+                rel="noopener noreferrer"
               >
                 <img
                   src="https://s3.amazonaws.com/shecodesio-production/uploads/files/000/084/786/original/8.png?1686168778"
@@ -93,6 +95,7 @@ function About() {
               <a
                 href="https://www.shecodes.io/certificates/2e9750e04026ee23ed0f5ca2d143f52e"
                 target="_blank"
+                rel="noopener noreferrer"
               >
                 <img
                   src="https://s3.amazonaws.com/shecodesio-production/uploads/files/000/084/787/original/6.png?1686169038"
@@ -103,6 +106,7 @@ function About() {
               <a
                 href="https://www.shecodes.io/certificates/52c6c8863207c60cc92e03cc33127f4e"
                 target="_blank"
+                rel="noopener noreferrer"
               >
                 <img
                   src="https://s3.amazonaws.com/shecodesio-production/uploads/files/000/084/788/original/4.png?1686169068"
@@ -113,6 +117,7 @@ function About() {
               <a
                 href="https://www.udemy.com/certificate/UC-89d13e8c-c724-40c0-8610-b9dd7439ae71/"
                 target="_blank"
+                rel="noopener noreferrer"
               >
                 <img
                   src="https://s3.amazonaws.com/shecodesio-production/uploads/files/000/096/157/original/udemy.png?1694132955"
@@ -130,6 +135,7 @@ function About() {
               <a
                 href="https://drive.google.com/file/d/1qBgbpYqgVg7L3k8eJbeCUNi-eZmimamK/view?usp=sharing"
                 target="_blank"
+                rel="noopener noreferrer"
               >
                 <img
                   src="https://s3.amazonaws.com/shecodesio-production/uploads/files/000/084/782/original/UI_UX_Designer_Certificate_Tetiana_Korchynska.jpg?1686168107"
@@ -140,6 +146,7 @@ function About() {
               <a
                 href="https://drive.google.com/file/d/1ujrqOH-qoEM2XcWcJR96U9k2RVNDF2vw/view?usp=sharing"
                 target="_blank"
+                rel="noopener noreferrer"
               >
                 <img
                   src="https://s3.amazonaws.com/shecodesio-production/uploads/files/000/084/783/original/UI_UX_Designer_Certificate_Tetiana_Korchynska_pt1.jpg?1686168113"
@@ -150,6 +157,7 @@ function About() {
               <a
                 href="https://drive.google.com/file/d/1s7qKLBHfCsrnwXnVg0-5XUlTbwytxMTZ/view?usp=sharing"
                 target="_blank"
+                rel="noopener noreferrer"
               >
                 <img
                   src="https://s3.amazonaws.com/shecodesio-production/uploads/files/000/084/784/original/UI_UX_Designer_Certificate_Tetiana_Korchynska_pt2.jpg?1686168119"
